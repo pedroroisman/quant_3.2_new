b3569126c8169385747c43f4532513bada3fe77f
pedroroisman <pedroroisman@gmail.com>
2025-07-19 11:51:40 -0300

    Comentar ruta index en app.py para evitar TemplateNotFound

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,7 @@ from flask import Flask, request, jsonify
 from quant_engine import run_quant_for_ticker
 import os
 from dotenv import load_dotenv
-from flask import render_template
 from flask_cors import CORS
-
 
 load_dotenv("tiingo.env")
 
@@ -34,9 +32,9 @@ def evaluate():
             'message': str(e)
         }), 500
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
